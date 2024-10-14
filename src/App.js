3dbcf30c8e08ff21b33f74c4dc9c6b1fcf923f6e
Sparkoms <sparkoms@gmail.com>
2024-10-14 16:55:57 +0300

    #3 Start

--- src/App.js
+++ src/App.js
@@ -1,13 +1,64 @@
+import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
-function App() {
-  return (
-    <div className="wrapper">
-      <Header />
-      <Footer />   
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Стул серый',
+          img: 'chair-grey.jpeg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'chairs',
+          price: '49.99'
+        },
+        {
+          id: 2,
+          title: 'Стол',
+          img: 'table.webp',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'tables',
+          price: '149.00'
+        },
+        {
+          id: 3,
+          title: 'Диван',
+          img: 'sofa.jpeg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'sofa',
+          price: '549.99'
+        },
+        {
+          id: 4,
+          title: 'Лампа',
+          img: 'wall-light.jpeg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'light',
+          price: '25.00'
+        },
+        {
+          id: 5,
+          title: 'Стул белый',
+          img: 'chair-white.jpeg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'chairs',
+          price: '49.99'
+        },
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <Footer />   
+      </div>
+    )
+  }
 }
 
 export default App;
