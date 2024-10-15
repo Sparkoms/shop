f82d2f0e9b1ce14e7714e9a805ea34f4c17d07e7
Sparkoms <sparkoms@gmail.com>
2024-10-16 00:59:06 +0300

    #3 homework

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Items from "./components/Items";
+
 
 class App extends React.Component {
   constructor(props) {
@@ -10,7 +12,7 @@ class App extends React.Component {
         {
           id: 1,
           title: 'Стул серый',
-          img: 'chair-grey.jpeg',
+          img: 'chair-grey.jpg',
           desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
           category: 'chairs',
           price: '49.99'
@@ -26,7 +28,7 @@ class App extends React.Component {
         {
           id: 3,
           title: 'Диван',
-          img: 'sofa.jpeg',
+          img: 'sofa.jpg',
           desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
           category: 'sofa',
           price: '549.99'
@@ -34,7 +36,7 @@ class App extends React.Component {
         {
           id: 4,
           title: 'Лампа',
-          img: 'wall-light.jpeg',
+          img: 'wall-light.jpg',
           desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
           category: 'light',
           price: '25.00'
@@ -42,7 +44,7 @@ class App extends React.Component {
         {
           id: 5,
           title: 'Стул белый',
-          img: 'chair-white.jpeg',
+          img: 'chair-white.jpg',
           desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
           category: 'chairs',
           price: '49.99'
@@ -55,6 +57,7 @@ class App extends React.Component {
     return (
       <div className="wrapper">
         <Header />
+        <Items items={this.state.items} />
         <Footer />   
       </div>
     )
