4d0b78748f77feebe08adcbf61ea64c3dd8d3da9
Sparkoms <sparkoms@gmail.com>
2024-10-17 00:14:58 +0300

    #4 cart css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { GiShoppingCart } from "react-icons/gi";
 
 export default function Header() {
+  let [cartOpen, setCartOpen] = useState(false);
   return (
     <header>
         <div>
@@ -10,6 +12,12 @@ export default function Header() {
               <li>Контакты</li>
               <li>Кабинет</li>
             </ul>
+            <GiShoppingCart onClick={() => setCartOpen(!cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`} />
+            {cartOpen && (
+              <div className='shop-cart'>
+                
+              </div>
+            )}
         </div>
         <div className='presentation'></div>
     </header>
