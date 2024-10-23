f1fa7392f22a7e974b84a082cdc56adbc193b8a9
Sparkoms <sparkoms@gmail.com>
2024-10-24 00:43:48 +0300

    #8 Detail information

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Items from "./components/Items";
 import Categories from "./components/Categories";
+import ShowFullItem from "./components/ShowFullItem";
 
 
 class App extends React.Component {
@@ -52,12 +53,15 @@ class App extends React.Component {
           category: 'chairs',
           price: '49.99'
         },
-      ]
+      ],
+      showFullItem: false,
+      fullItem: {}
     }
     this.state.currentItems = this.state.items
     this.addToOrder = this.addToOrder.bind(this)
     this.deleteOrder = this.deleteOrder.bind(this)
     this.chooseCategory = this.chooseCategory.bind(this)
+    this.onShowItem = this.onShowItem.bind(this)
   }
 
   render() {
@@ -65,14 +69,27 @@ class App extends React.Component {
       <div className="wrapper">
         <Header orders={this.state.orders} onDelete={this.deleteOrder} />
         <Categories chooseCategory={this.chooseCategory} />
-        <Items items={this.state.currentItems} onAdd={this.addToOrder} />
+        <Items onShowItem={this.onShowItem} items={this.state.currentItems} onAdd={this.addToOrder} />
+
+        {this.state.showFullItem && <ShowFullItem onShowItem={this.onShowItem} onAdd={this.addToOrder} item={this.state.fullItem} />}
         <Footer />   
       </div>
     )
   }
 
+  onShowItem(item) {
+    this.setState({fullItem: item})
+    this.setState({showFullItem: !this.state.showFullItem})
+  }
+
   chooseCategory(category) {
-    console.log(category)
+    if(category === 'all') {
+      this.setState({currentItems: this.state.items})
+      return
+    }
+    this.setState({
+      currentItems: this.state.items.filter(element => element.category === category)
+    })
   }
 
   addToOrder(item) {
